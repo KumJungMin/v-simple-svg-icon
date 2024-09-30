bb05279719bdd1f9fff58dbd41fc2ef1dc5b6bc5
KumJungMin <today.as.fresh@gmail.com>
2024-09-30 17:10:44 +0900

    chore: add test sample

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-import { provide } from 'vue';
+import { provide, ref } from 'vue';
 import Icon from "./components/icons";
 import { useSvgCacheStore } from "./composables/useSvgCacheStore"; // 캐시 스토어 컴포저블 경로
 
 // 캐시 스토어를 생성
 const svgCacheStore = useSvgCacheStore();
-
+const showIconOne = ref(true);
+const showIconTwo = ref(true);
 
 // 캐시 스토어를 전역적으로 제공
 provide("svgCacheStore", svgCacheStore);
@@ -13,12 +14,13 @@ provide("svgCacheStore", svgCacheStore);
 
 <template>
   <div>
-    <Icon name="sample" width="50" height="50" color="red" hover-color="pink" />
-    <Icon name="sample" width="50" height="50" color="blue" hover-color="gray" />
+    <button @click="showIconOne = !showIconOne">아이콘 토글1</button>
+    <button @click="showIconTwo = !showIconTwo">아이콘 토글2</button>
+    <Icon v-if="showIconOne" name="sample" width="50" height="50" color="red" hover-color="pink" />
+    <Icon v-if="showIconTwo" name="sample" width="50" height="50" color="blue" hover-color="gray" />
     <Icon name="sample2" width="150" height="250" :color="['orange', 'red']" :hover-color="['blue', 'black']" />
     <Icon name="sample2" width="150" height="250" :color="['pink', 'white']" :hover-color="['gray', 'gray']" />
     
-    {{svgCacheStore.svgCache}}
     {{svgCacheStore.iconUsageCount}}
 
 </div>
---
+++ src/components/TableSample.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+      <table :border="1">
+        <thead>
+          <tr>
+            <th>키 (Key)</th>
+            <th>값 (Value)</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(value, key) in props.obj" :key="key">
+            <td>{{ key }}</td>
+            <td>{{ value }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </template>
+  
+  <script setup>
+  const props = defineProps({
+        obj: Object
+  });
+  </script>
+  
+  <style scoped>
+  table {
+    width: 100%;
+    border-collapse: collapse;
+  }
+  
+  th, td {
+    padding: 8px;
+    text-align: left;
+  }
+  </style>
+  
