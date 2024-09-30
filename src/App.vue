7401645f3cfb9d982e0e49f3776732dfbaa53c62
KumJungMin <today.as.fresh@gmail.com>
2024-09-30 17:01:47 +0900

    chore: add example

--- src/App.vue
+++ src/App.vue
@@ -6,18 +6,20 @@ import { useSvgCacheStore } from "./composables/useSvgCacheStore"; // 캐시 스
 // 캐시 스토어를 생성
 const svgCacheStore = useSvgCacheStore();
 
+
 // 캐시 스토어를 전역적으로 제공
 provide("svgCacheStore", svgCacheStore);
 </script>
 
 <template>
   <div>
-    <!-- <img src="./assets/sample.svg" /> -->
     <Icon name="sample" width="50" height="50" color="red" hover-color="pink" />
     <Icon name="sample" width="50" height="50" color="blue" hover-color="gray" />
-    <!-- <Icon name="sample2" width="150" height="250" color="orange" hover-color="red" />
-    <Icon name="sample2" width="150" height="250" color="blue" hover-color="black" /> -->
     <Icon name="sample2" width="150" height="250" :color="['orange', 'red']" :hover-color="['blue', 'black']" />
+    <Icon name="sample2" width="150" height="250" :color="['pink', 'white']" :hover-color="['gray', 'gray']" />
+    
+    {{svgCacheStore.svgCache}}
+    {{svgCacheStore.iconUsageCount}}
 
 </div>
 </template>
